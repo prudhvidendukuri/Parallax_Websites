653218f23a923560ced194bd769402f5e347359c
Ravi Varma <prudhviddsp@gmail.com>
2023-05-20 13:19:08 +0530

    added second websites

---
+++ second_website/script.js
@@ -0,0 +1,17 @@
+
+// let moveover = document.getElementById('mouse');
+// let transp = document.getElementById('transparent');
+
+
+// document.addEventListener("mousemove", function (event) {
+//     var cursorX = event.clientX;
+//     var cursorY = event.clientY;
+//     if (cursorY >= 112) {
+//         moveover.style.display = 'block';
+//         moveover.style.top = cursorY - 50 + 'px';
+//         moveover.style.left = cursorX - 50  + 'px';
+//     }else{
+//         moveover.style.display = 'none';
+//     }
+
+// });
